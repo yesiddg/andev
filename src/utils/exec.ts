058737a57d785f808a58ed9b1527e6ee2d6efe41
yesid <yeligoth@gmail.com>
2026-02-05 20:36:06 -0500

    Fix build errors

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,7 +1,11 @@
-import execa from 'execa'
+import { execa } from 'execa'
 import chalk from 'chalk'
 
 export async function exec(command: string) {
   console.log(chalk.cyan(`➜ ${command}`))
-  await execa.command(command, { stdio: 'inherit' })
+
+  await execa(command, {
+    stdio: 'inherit',
+    shell: true
+  })
 }
